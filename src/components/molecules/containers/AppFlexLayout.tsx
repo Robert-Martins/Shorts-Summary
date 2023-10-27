296ec9009fa047b62bd06d8cb4c9e721d6a106ca
Robert-Martins <robertcantares@gmail.com>
2023-10-27 20:20:37 -0300

    feat/enhance-container-flex-and-grid-components

--- src/components/molecules/containers/AppFlexLayout.tsx
+++ src/components/molecules/containers/AppFlexLayout.tsx
@@ -1,7 +1,19 @@
 import styled from "styled-components";
 
-const AppFlexLayout = styled.div`
+type FlexLayoutProps = {
+    $column?: boolean;
+    justify?: string;
+    align?: string;
+    gap?: number;
+}
 
+const AppFlexLayout = styled.div<FlexLayoutProps>`
+    display: flex;
+    flex-direction: ${props => props?.$column ? 'column' : 'row'};
+    justify-content: ${props => props?.justify ? props?.justify : 'center'};
+    align-items: ${props => props?.align ? props?.align : 'center'};
+    gap: ${props => props.gap ? props.gap : 12}px;
+    flex-wrap: nowrap;
 `;
 
 export default AppFlexLayout;
--- src/components/molecules/containers/AppGrid.tsx
+++ src/components/molecules/containers/AppGrid.tsx
@@ -1,7 +1,17 @@
 import styled from "styled-components";
 
-const AppGrid = styled.div`
+type GridProps = {
+    columns?: number;
+    gap?: number;
+    rowGap?: number;
+    columnGap?: number;
+}
 
+const AppGrid = styled.div<GridProps>`
+    display: grid;
+    grid-template-columns: repeat(${props => props?.columns ? props.columns : 2}, 1fr);
+    row-gap: ${props => props?.rowGap ? props.rowGap : 40}px;
+    column-gap: ${props => props?.columnGap ? props.columnGap : 40}px;
 `;
 
 export default AppGrid;
