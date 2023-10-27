634c97269ae9dc5d34f2f718a98a8ec6e49d940c
Robert-Martins <robertcantares@gmail.com>
2023-10-27 20:22:20 -0300

    feat/home-presentation-section-finish

--- src/components/organisms/sections/Presentation.tsx
+++ src/components/organisms/sections/Presentation.tsx
@@ -1,14 +1,39 @@
 import styled from "styled-components";
+import { AppFlexLayout } from "../..";
 
 const StyledPresentation = styled.section`
-
+    flex: 0.6;
+    padding-left: 8rem;
+    > div {
+        h1 {
+            color: var(--white);
+        }
+        h2 {
+            color: var(--gray);
+        }
+        img {
+            &:first-of-type {
+                height: 4.5rem;
+            }
+            &:nth-of-type(2) {
+                height: 7.5rem;
+            }
+        }
+    }
 `;
 
-const Presentation: React.FunctionComponent<void> = () => {
+const Presentation: React.FunctionComponent = () => {
 
     return (
         <StyledPresentation>
-
+            <AppFlexLayout $column justify="space-between" align="flex-start" gap={32}>
+                <img src="src/assets/logos/rocketseat.svg"/>
+                <h1>
+                    Shorts Summary
+                </h1>
+                <h2>Trilha Foundations</h2>
+                <img src="src/assets/logos/nlw.svg"/>
+            </AppFlexLayout>
         </StyledPresentation>
     );
 
--- src/components/views/Home.tsx
+++ src/components/views/Home.tsx
@@ -1,9 +1,34 @@
-const Home: React.FunctionComponent<void> = () => {
+import styled from "styled-components";
+import { AppFlexLayout, Main, Presentation } from "..";
 
+const StyledHome = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background-color: black;
+    > div {
+        &:first-of-type {
+            height: 97.5%;
+            flex-basis: 1;
+        }
+        &:nth-of-type(2) {
+            height: 2.5%;
+            background: linear-gradient(90deg, #0B0726 0%, #342C66 24.96%, #7142C9 49.91%, #A29AF5 83.85%, #DEE0FC 99.82%);
+        }
+    }
+`;
+
+const Home: React.FunctionComponent = () => {
     return (
-        <></>
+        <StyledHome>
+            <AppFlexLayout justify="center" align="center" gap={0}>
+                <Presentation />
+                <Main />
+            </AppFlexLayout>
+            <div/>
+        </StyledHome>
     );
-
-};
+}
 
 export default Home;
